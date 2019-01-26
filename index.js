98793c1ae89fbef5ca7de3438c4ee4765a408f2c
Joe Corwin <joe.corwin@gmail.com>
2019-01-26 04:30:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,69 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var randNum = Math.floor((Math.random() * 100)+1);
+  var newItem = {
+    itemName: item,
+    itemPrice: randNum,
+  };
+ if (cart.push(newItem)) {
+   return `${item} has been added to your cart.`
+ }
 }
+
 
 function viewCart() {
-  // write your code here
-}
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  var output = "In your cart, you have "
+  for(var i = 0; i < cart.length; i++) {
+    if (cart.length === 1) {
+      output += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+    } else if (cart.length === 2) {
+        if (i === 0) {
+          output += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `
+        } else {
+          output += `and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+        }
+    } else {
+        if (i === 0) {
+          output += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `
+        } else if (i !== cart.length - 1) {
+          output += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `
+        } else {
+          output += `and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+        }
+      }
+    }
+    return output
+  }
+
 
 function total() {
-  // write your code here
+  let priceArray = cart.map(a => a.itemPrice);
+  var sum = priceArray.reduce((a, b) => a + b, 0);
+  return sum
 }
+
 
 function removeFromCart(item) {
-  // write your code here
-}
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].itemName === item) {
+        cart.splice(i,1)
+        return cart
+      }
+    }
+    return "That item is not in your cart."
+  }
+
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+    if (cardNumber === undefined) {
+      return "Sorry, we don't have a credit card on file for you."
+    } else {
+      let t = total()
+      cart = []
+      return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
+    }
+  }
