59f168ec2b446971fdc95632ac1f1f7a79ce4b99
Joe Corwin <joe.corwin@gmail.com>
2019-01-26 04:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ function placeOrder(cardNumber) {
     } else {
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     }
-      setCart()
+      cart = []
   }  
